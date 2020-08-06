0f192554f84f119c5699c87ccbe1a6c0d07dbd7e
Desiree Marquez <marquezdecardona@gmail.com>
2020-08-06 01:32:06 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,26 @@
+class Artist
+  
+  @@all=[ ]
+  
+  attr_accessor :name, :genres
+  
+  def initialize(name)
+    @name=name
+    @@all<< self
+    @genres= genres
+    
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def new_song(name, genre)
+    Song.new(name, self, genre)
+  end
+  
+  def songs
+    Song.all.select {|songs| song.artist == self}
+  end
+  
+end
