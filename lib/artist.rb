ff247ebfdd53529640f1769f6d76bb1ac4611a20
Desiree Marquez <marquezdecardona@gmail.com>
2020-08-06 02:56:01 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -25,7 +25,7 @@ class Artist
     #Song.new = name, self, genre)
     #song.new= :name(:genre)
     #ninety_nine_problems = jay_z.new_song("Ninety Nine Problems", rap)
-   end
+   
    
   # def add_song(song)
   #   song.artist = self
