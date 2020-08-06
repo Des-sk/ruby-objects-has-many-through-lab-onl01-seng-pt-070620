f924a8ae119e7822be7c2e7a0180ac34918e2ed6
Desiree Marquez <marquezdecardona@gmail.com>
2020-08-06 02:50:03 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -15,7 +15,8 @@ class Artist
   end
   
   def new_song(name, genre)
-    Song.new(name, self, genre)
+    name+genre=Song.new
+    song.new<< self
     #Song.new
     #song.new = self
     #Song.new = name, self, genre)
