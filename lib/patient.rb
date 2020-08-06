57d8ceec204a8862d7955d7dc18ee4ec7ea11745
Desiree Marquez <marquezdecardona@gmail.com>
2020-08-06 04:51:35 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -9,7 +9,18 @@ class Patient
   end
  
  def new_appointment(date, doctor) 
-   Appointment.new
+   Appointment.new(date, doctor, self)
+ end
+ 
+ def self.all
+   @@all
+ end
+ 
+ def appointments 
+   Appointment.all {|appointments| appointment.patient == self}
+ end
+ 
+ 
 
   
  
