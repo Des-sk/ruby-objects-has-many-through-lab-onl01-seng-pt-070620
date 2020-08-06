9a6c443f7b5780a16a0b76008678bb4ce9b253da
Desiree Marquez <marquezdecardona@gmail.com>
2020-08-06 03:22:11 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -14,39 +14,16 @@ class Artist
     @@all
   end
   
-  def new_song(name, genre)
-    #new_song = self.new
-    new_song.name.genre=self
+  def new_song(name, genres)
+    
   end
     
-  #   def self.new_by_filename(filename)
-  #   artist, song = filename.split(" - ")
-  #   new_song = self.new(song)
-  #   new_song.artist_name = artist
-  #   new_song
-  # end
-    #Song.new
-    #song.new = self
-    #Song.new = name, self, genre)
-    #song.new= :name(:genre)
-    #ninety_nine_problems = jay_z.new_song("Ninety Nine Problems", rap)
-   
-   
-  # def add_song(song)
-  #   song.artist = self
-  # end
-
+  
   
   def songs
     Song.all.select {|songs| songs.artist == self}
   end
  
- def genre 
-   songs.map(&:genres)
- end
-   
-  # def artists
-  #   songs.map(&:artist)
-  # end
+ 
  
 end
